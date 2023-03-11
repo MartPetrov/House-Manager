ddcaa37c9b518d92193c00dc5e01c7c282e51b8b
Martin Petrov <95379693+MartPetrov@users.noreply.github.com>
2023-03-11 20:34:02 +0200

    init Commit

---
+++ src/main/java/project/HouseManageApplication.java
@@ -0,0 +1,13 @@
+package project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseManageApplication.class, args);
+	}
+
+}
