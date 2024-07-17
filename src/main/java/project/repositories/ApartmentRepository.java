5e1713a2cdce14b7f895e73ae8eb8b52c86ef11a
Martin Petrov <Martinpetrow93@gmail.com>
2024-07-18 00:41:45 +0300

    Add Apartment class like entity

--- src/main/java/project/repositories/ApartmentRepository.java
+++ src/main/java/project/repositories/ApartmentRepository.java
@@ -3,7 +3,6 @@ package project.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.entity.Apartment;
-import project.entity.People;
 
 import java.util.List;
 import java.util.Optional;
