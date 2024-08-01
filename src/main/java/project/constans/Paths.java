5017274b04ca97c2995147b39de728ba6ffd40f6
Martin Petrov <Martinpetrow93@gmail.com>
2024-08-02 02:43:10 +0300

    Fix some titles

--- src/main/java/project/constans/Paths.java
+++
@@ -1,9 +0,0 @@
-package project.constans;
-
-import java.io.File;
-import java.nio.file.Path;
-
-public enum Paths {
-    ;
-    public static final Path TXT_PEOPLE = Path.of("src/main/resources/files/xml/Peoples.txt");
-}
