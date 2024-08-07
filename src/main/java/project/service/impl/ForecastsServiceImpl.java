e9d80cad46c5f3e3c74b8343bacdf7a2c2f8ed40
Martin Petrov <Martinpetrow93@gmail.com>
2024-08-07 23:17:27 +0300

    Final commit

--- src/main/java/project/service/impl/ForecastsServiceImpl.java
+++ src/main/java/project/service/impl/ForecastsServiceImpl.java
@@ -1,10 +1,13 @@
 package project.service.impl;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import project.model.dto.WeatherForecastDTO;
 import project.service.ForecastsService;
+import project.service.exception.ObjectNotFoundException;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -23,9 +26,9 @@ public class ForecastsServiceImpl implements ForecastsService {
                 .uri(uriBuilder -> uriBuilder.path("/forecast")
                         .queryParam("city", city)
                         .build()).accept(MediaType.APPLICATION_JSON)
-                .retrieve()
+                .retrieve().onStatus(s -> s.isSameCodeAs(HttpStatus.NOT_FOUND), clientResponse ->
+                        Mono.error(new ObjectNotFoundException("Forecast for this city:  " + city, city)))
                 .bodyToMono(WeatherForecastDTO.class).block(Duration.ofSeconds(5));
-
 
         return forecastDTOMono;
     }
